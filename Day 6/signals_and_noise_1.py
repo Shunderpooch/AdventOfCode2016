eb3f14587330f25d3f134524fda0fbf756cdf926
Arthur Dooner <shunderpooch@gmail.com>
2017-06-03 03:52:35 -0400

    Advent of Code Day 6 Challenge, Complete

---
+++ Day 6/signals_and_noise_1.py
@@ -0,0 +1,33 @@
+"""
+Arthur Dooner
+Advent of Code Day 6
+Challenge 1
+"""
+
+import sys
+
+COLUMN_DICT_LIST = []
+INIT = True
+with open("garbledmessage.txt", "r") as filestream:
+    for line in filestream:
+        TEMP_LINE_INPUT = line.rstrip()
+        if INIT:
+            for character in TEMP_LINE_INPUT:
+                TEMP_DICT = {}
+                COLUMN_DICT_LIST.append(TEMP_DICT)
+                INIT = False
+        temp_counter = 0
+        for character in TEMP_LINE_INPUT:
+            COLUMN_DICT_LIST[temp_counter][character] = COLUMN_DICT_LIST[temp_counter].get(character, 0) + 1
+            temp_counter += 1
+
+STRING_RESULT = ""
+for dictionary in COLUMN_DICT_LIST:
+    # print(dictionary)
+    temp_list = sorted(dictionary, key=dictionary.__getitem__, reverse=True)
+    # print(temp_list)
+    if temp_list:
+        STRING_RESULT += temp_list[0]
+
+print("The message is: " + STRING_RESULT)
+sys.stdout.flush()
